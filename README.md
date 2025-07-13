d799b2470346e96a85d19a24b85e8de83addf5e3
Azxcc0 <2745285962@qq.com>
2025-07-13 22:57:46 +0800

    feat: 实现项目结构和引入inih库，并实现测试代码

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ Currently, the main focus is on updating
 
 # Open source library
 I will learn by adding comments to the open-source libraries I am currently studying, incorporating test programs, and using xmake for project management
+
+
+# inih
+
+inih is a simple .INI file parser written in C. It is designed to be small, compact, portable, and efficient. It is used in many applications, including some of the most popular Linux applications.
+
+website: https://github.com/benhoyt/inih.git
---
+++ xmake.lua
@@ -0,0 +1,30 @@
+set_project("Live")
+set_version("1.0.0", {build = "%Y%m%d%H%M"})
+set_description("Random Notes from Sickbed")
+set_objectdir("build/$(plat)/$(arch)/$(mode)")
+set_targetdir("bin/$(plat)/$(arch)/$(mode)")
+
+
+add_rules("mode.debug", "mode.release")
+
+add_cxxflags("-std=c++17") 
+add_cxflags("-Wno-error=deprecated-declarations", "-fno-strict-aliasing", "-Wno-error=expansion-to-defined")
+add_mxflags("-Wno-error=deprecated-declarations", "-fno-strict-aliasing", "-Wno-error=expansion-to-defined")
+set_warnings("all", "error")
+
+if is_plat("windows") then
+    add_defines("PT_WIN")
+elseif is_plat("linux") then
+    add_defines("PT_LNX")
+end
+
+if is_mode("debug") then
+    add_cxxflags("-g", "-O0") 
+elseif is_mode("release") then
+    add_cxxflags("-O3")       
+    add_ldflags("-s")         
+end
+
+add_includedirs("include")
+includes("src/ini")
+includes("example")
